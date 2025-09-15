a9d579808cd07bb123378813027107d0dd626112
Owner <stam1@gmail.com>
2025-09-15 16:30:46 +0300

    Add project files.

---
+++ ExceptionLesson/Fraction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionLesson
+{
+	internal class Fraction
+	{
+		//private int nominator;//מונה	
+		private int denominator;//מכנה
+
+		public int Nominator
+		{
+			get;set;
+		}
+
+		public int Denominator
+		{
+			get
+			{
+				return denominator;
+			}
+			set
+			{
+				if (value != 0)
+					denominator = value;
+			}
+		}
+		public Fraction(int nominator, int denominator)
+		{
+			Nominator = nominator;
+			Denominator = denominator;
+		}
+		public override string ToString()
+		{
+			return $"{Nominator}/{Denominator}";
+		}
+		public double Result()
+		{
+			return (double)Nominator / Denominator;
+		}
+
+		public int Modulu()
+		{
+			return Nominator % Denominator;
+		}
+	}
+}
---
+++ ExceptionLesson/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ExceptionLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Fraction f1 = new Fraction(6, 3);
+			Console.WriteLine("Enter nominator");
+            string str = null;
+            bool validInput = false;
+            while (!validInput)
+            {
+                try
+                {
+                    num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Denominator");
+                    num2 = int.Parse(Console.ReadLine());
+               //     int num3 = int.Parse(str);
+                    f1 = new Fraction(num1, num2);
+                    validInput = true;
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine("Null argument enter new numbers");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("only numbers");
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("number too big");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
+			Console.WriteLine(f1);
+            Console.WriteLine($"{f1}={f1.Result()}");
+        }
+    }
+}
